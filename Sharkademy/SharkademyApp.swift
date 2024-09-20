c1030292465fae26f6439d4b7c878444e395072d
itsFloppyhat <mlucas24@icloud.com>
2024-09-20 19:29:00 -0400

    Initial Commit

---
+++ Sharkademy/SharkademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharkademyApp.swift
+//  Sharkademy
+//
+//  Created by Matthew Lucas on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SharkademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
